11216555913893dae94148016595626ecb6b014f
shashank123456755776 <114920880+shashank123456755776@users.noreply.github.com>
2025-07-31 16:13:29 +0530

    ch

--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include("ecommerceapp.urls")),
     path('auth/',include("authcart.urls"))
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
