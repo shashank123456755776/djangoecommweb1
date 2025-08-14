70bf77640f3867143beea95ffb1feb7026bd79a1
shashank123456755776 <114920880+shashank123456755776@users.noreply.github.com>
2025-08-14 11:23:06 +0530

    commit 12

--- ecommerce/authcart/views.py
+++ ecommerce/authcart/views.py
@@ -8,6 +8,8 @@ from .utils import TokenGenerator,generate_token
 from django.utils.encoding import force_bytes, force_str
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 from django.utils.encoding import DjangoUnicodeDecodeError
+from django.urls import reverse   # ✅ Added
+
 
 from django.core.mail import EmailMessage
 from django.conf import settings
@@ -15,38 +17,75 @@ from django.conf import settings
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
 from django.contrib.auth import authenticate,login,logout
 # Create your views here.
-def signup(request):
-    if request.method=="POST":
-        email=request.POST['email']
-        password=request.POST['pass1']
-        confirm_password=request.POST['pass2']
-        if password!=confirm_password:
-            messages.warning(request,"Password is Not Matching")
-            return render(request,'signup.html')                   
-        try:
-            if User.objects.get(username=email):
-                # return HttpResponse("email already exist")
-                messages.info(request,"Email is Taken")
-                return render(request,'signup.html')
-        except Exception as identifier:
-            pass
-        user = User.objects.create_user(email,email,password)
-        user.is_active=False
-        user.save()
-        email_subject="Activate Your Account"
-        message=render_to_string('activate.html',{
-            'user':user,
-            'domain':'https://djangoecommweb1-11.onrender.com/',
-            'uid':urlsafe_base64_encode(force_bytes(user.pk)),
-            'token':generate_token.make_token(user)
+# def signup(request):
+#     if request.method=="POST":
+#         email=request.POST['email']
+#         password=request.POST['pass1']
+#         confirm_password=request.POST['pass2']
+#         if password!=confirm_password:
+#             messages.warning(request,"Password is Not Matching")
+#             return render(request,'signup.html')                   
+#         try:
+#             if User.objects.get(username=email):
+#                 # return HttpResponse("email already exist")
+#                 messages.info(request,"Email is Taken")
+#                 return render(request,'signup.html')
+#         except Exception as identifier:
+#             pass
+#         user = User.objects.create_user(email,email,password)
+#         user.is_active=False
+#         user.save()
+#         email_subject="Activate Your Account"
+#         message=render_to_string('activate.html',{
+#             'user':user,
+#             'domain':'https://djangoecommweb1-11.onrender.com',
+#             'uid':urlsafe_base64_encode(force_bytes(user.pk)),
+#             'token':generate_token.make_token(user)
 
+#         })
+
+#         email_message = EmailMessage(email_subject,message,settings.EMAIL_HOST_USER,[email])
+#         email_message.send()
+#         messages.success(request,f"Activate Your Account by clicking the link in your gmail {message}")
+#         return redirect('/auth/login/')
+#     return render(request,"signup.html")
+def signup(request):
+    if request.method == "POST":
+        email = request.POST['email']
+        password = request.POST['pass1']
+        confirm_password = request.POST['pass2']
+
+        if password != confirm_password:
+            messages.warning(request, "Passwords do not match")
+            return render(request, 'signup.html')
+
+        if User.objects.filter(username=email).exists():
+            messages.info(request, "Email is already taken")
+            return render(request, 'signup.html')
+
+        user = User.objects.create_user(email, email, password)
+        user.is_active = False
+        user.save()
+
+        # Build correct activation link
+        uid = urlsafe_base64_encode(force_bytes(user.pk))
+        token = generate_token.make_token(user)
+        activation_link = request.build_absolute_uri(
+            reverse('activate', kwargs={'uidb64': uid, 'token': token})
+        )
+
+        email_subject = "Activate Your Account"
+        message = render_to_string('activate.html', {
+            'user': user,
+            'activation_link': activation_link
         })
 
-        email_message = EmailMessage(email_subject,message,settings.EMAIL_HOST_USER,[email])
+        email_message = EmailMessage(email_subject, message, settings.EMAIL_HOST_USER, [email])
         email_message.send()
-        messages.success(request,f"Activate Your Account by clicking the link in your gmail {message}")
+
+        messages.success(request, f"Activate your account by clicking the link sent to {email}")
         return redirect('/auth/login/')
-    return render(request,"signup.html")
+    return render(request, "signup.html")
 
 
 class ActivateAccountView(View):
